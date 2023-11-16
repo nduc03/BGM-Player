138e9d9b6e423e15c8222b3b7e70be185e37b594
Duc <48994241+nduc03@users.noreply.github.com>
2023-11-17 01:14:09 +0700

    Remove some unnecessary code in VorbisLoopStream

--- BgmPlayer/Audio/VorbisLoopStream.cs
+++ BgmPlayer/Audio/VorbisLoopStream.cs
@@ -1,32 +1,13 @@
 ﻿using NAudio.Vorbis;
-using NAudio.Wave;
 
 namespace bgmPlayer
 {
     // Inspired by NAudio.Extras.LoopStream but modified for VorbisWave
+    // Modified for always enable looping and removed some unnecessary field
     public class VorbisLoopStream : VorbisWaveReader
     {
         public VorbisLoopStream(string path) : base(path)
         {
-            EnableLooping = true;
-        }
-
-        public bool EnableLooping { get; set; }
-
-        public override WaveFormat WaveFormat
-        {
-            get { return base.WaveFormat; }
-        }
-
-        public override long Length
-        {
-            get { return base.Length; }
-        }
-
-        public override long Position
-        {
-            get { return base.Position; }
-            set { base.Position = value; }
         }
 
         public override int Read(byte[] buffer, int offset, int count)
@@ -38,10 +19,6 @@ namespace bgmPlayer
                 int bytesRead = base.Read(buffer, offset + totalBytesRead, count - totalBytesRead);
                 if (bytesRead == 0)
                 {
-                    if (base.Position == 0 || !EnableLooping)
-                    {
-                        break;
-                    }
                     base.Position = 0;
                 }
                 totalBytesRead += bytesRead;
